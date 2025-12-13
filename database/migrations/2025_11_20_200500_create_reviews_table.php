24eefdcd228ff609e3ec573b7ef9219cfb370506
iMercy <mercybrightmassang@gmail.com>
2025-12-13 02:01:07 -0700

    fix: Correct migration path in service provider

--- database/migrations/2025_11_20_200500_create_reviews_table.php
+++ database/migrations/2025_11_20_200500_create_reviews_table.php
@@ -38,6 +38,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('reviews');er
     }
 };
--- src/ReviewsServiceProvider.php
+++ src/ReviewsServiceProvider.php
@@ -13,6 +13,6 @@ class ReviewsServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
